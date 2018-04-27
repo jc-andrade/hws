34c3836b2aa372cdb99e580bda18311836d0fe5c
Julio C Andrade-Merrow <jcamvz@gmail.com>
2018-04-27 11:26:01 -0500

    added project2 skeleton

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Julio Cesar Andrade-Merrow on 4/27/18.
+//  Copyright © 2018 Julio Cesar Andrade-Merrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
