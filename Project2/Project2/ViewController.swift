405880746bd364d7f3cfb9e4a885d1cec7e67688
Julio C Andrade-Merrow <jcamvz@gmail.com>
2018-04-27 14:53:49 -0500

    Project 2 finalized, game works

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -36,29 +36,42 @@ class ViewController: UIViewController {
         button3.layer.borderWidth = 1
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        
-        // This statement automatically randomizes the order
-        // of the countries in the array
-        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
-        
         // Call askQuestion method
         askQuestion()
         
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         
-        title = countries[correctAnswer].uppercased()
-       
+        // This statement automatically randomizes the order
+        // of the countries in the array
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
         // two parameters, UIimage, for: .normal means 'standard state of button'
         button1.setImage(UIImage(named:countries[0]), for: .normal)
         button2.setImage(UIImage(named:countries[1]), for: .normal)
         button3.setImage(UIImage(named:countries[2]), for: .normal)
         
         correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "You got it, you dingo!"
+            score += 1
+        } else {
+            title = "Idiot!!!!!!!!"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        // Handler: code that it can execute when the button is tapped. You can also write customer code in there, but we just wanna continue so we call askQuestion
+        present(ac, animated: true)
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
