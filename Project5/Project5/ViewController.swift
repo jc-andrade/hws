bf7aeb446184be7196e382c2769258981b4f5fd9
Julio C Andrade-Merrow <jcamvz@gmail.com>
2018-05-10 15:03:32 -0500

    Added skeleton for project 5

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Julio Cesar Andrade-Merrow on 5/10/18.
+//  Copyright © 2018 Julio Cesar Andrade-Merrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
